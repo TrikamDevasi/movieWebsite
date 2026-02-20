e85e3576a9f57a29322bc69add5250fdbc9688e8
TrikamDevasi <trikam.devasi.cg@outlook.com>
2026-02-20 15:23:10 +0530

    feat(utils): add streaming provider logos map

---
+++ utils/providerLogos.js
@@ -0,0 +1,24 @@
+// utils/providerLogos.js
+// TMDB Watch Provider ID → Hosted logo URLs
+// Source: Icons8 / brand CDNs (square, transparent, safe sizes)
+
+export const PROVIDER_LOGOS = {
+  // Global platforms
+  8: "https://img.icons8.com/color/48/netflix.png",              // Netflix
+  9: "https://img.icons8.com/color/48/amazon-prime-video.png",   // Prime Video
+  337: "https://img.icons8.com/color/48/disney-plus.png",        // Disney+
+  15: "https://img.icons8.com/color/48/hulu.png",                // Hulu
+  384: "https://img.icons8.com/color/48/hbo-max.png",            // HBO Max
+  2: "https://img.icons8.com/color/48/apple-tv.png",             // Apple TV+
+  350: "https://img.icons8.com/color/48/youtube-play.png",       // YouTube Movies
+  419: "https://img.icons8.com/color/48/paramount-plus.png",     // Paramount+
+  3844: "https://img.icons8.com/color/48/peacock.png",           // Peacock
+  389: "https://img.icons8.com/color/48/tubi.png",               // Tubi
+  278: "https://img.icons8.com/color/48/crunchyroll.png",        // Crunchyroll
+
+  // India-focused platforms
+  122: "https://img.icons8.com/color/48/hotstar.png",            // Disney+ Hotstar
+  121: "https://img.icons8.com/color/48/zee5.png",               // ZEE5
+  220: "https://img.icons8.com/color/48/jiocinema.png",          // JioCinema
+  237: "https://img.icons8.com/color/48/sony-liv.png",           // Sony LIV
+};
